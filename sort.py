40086ca90be297a7fd9de5d9ca30b3f93ba8fa59
Pratik Ghimire <63243723+prateekghimire@users.noreply.github.com>
2020-11-22 15:17:50 +0545

    Exception handling if file with same name exists

--- sort.py
+++ sort.py
@@ -11,10 +11,19 @@ for item in files:
     if extension not in unique_extensions:
         unique_extensions.append(extension)
 
+for item in unique_extensions:
+    try:
+        os.mkdir("{}".format(item))
+        print('New directory created')
+    except Exception as e:
+        print("Directory already exists")
 
 for item in files:
     extension=item.split('.')
     extension=extension[len(extension)-1]
     for file_ in unique_extensions:
         if file_==extension:
-            shutil.move(item,extension)
+            try:
+                shutil.move(item,extension)
+            except Exception as e:
+                print("File with same name exists")
